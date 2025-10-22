e99913b52234db238c18eaf93f8fd6ddfac7b612
gyunho <gyunh01@pukyong.ac.kr>
2025-10-22 16:24:14 +0900

    feat(Car): 자동차는 전진 조건을 만족하면 움직인다

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,12 +3,22 @@ package racingcar;
 public class Car {
 
     private final String name;
-    private Integer position;
+    private int position;
 
     Car(String name) {
         validateName(name);
         this.name = name;
         this.position = 0;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int condition) {
+        if (condition >= 4) {
+            this.position += 1;
+        }
     }
 
     private void validateName(String name) {
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -10,6 +10,19 @@ import org.junit.jupiter.params.provider.ValueSource;
 class CarTest {
 
     @ParameterizedTest
+    @ValueSource(ints = {4, 5, 9, 100})
+    void 전진_조건을_만족하면_움직인다(int condition) {
+        //given
+        Car car = new Car("gyun");
+
+        //when
+        car.move(condition);
+
+        //then
+        assertEquals(1, car.getPosition());
+    }
+
+    @ParameterizedTest
     @NullSource
     @ValueSource(strings = {"abcdef", "", "toooooo long name"})
     void 유효하지_않은_이름이_전달되면_예외를_던진다(String name) {
