07b037d1951daa37baa996ec2396dda6e76a1dbd
gyunho <gyunh01@pukyong.ac.kr>
2025-10-23 12:10:10 +0900

    test(Game): 전진하는 조건이 4이상일 때 자동차의 움직임을 검증한다

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -13,7 +14,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 class GameTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {100_001, 1_000_001, Integer.MAX_VALUE})
+    @ValueSource(ints = {100_001, Integer.MAX_VALUE})
     void 최대_랩_수를_넘으면_예외를_던진다(int labs) {
         List<Car> cars = List.of(new Car("gyun"));
         assertThrows(IllegalArgumentException.class, () -> new Game(labs, cars));
@@ -38,5 +39,20 @@ class GameTest {
         game.nextLab();
 
         assertEquals(labs - 1, game.getRemainingLaps());
+    }
+
+    @Test
+    void 전진하는_조건은_값이_4_이상일_경우이다() {
+        int labs = 10;
+        Car car1 = new Car("gyun");
+        Car car2 = new Car("ho");
+        List<Car> cars = List.of(car1, car2);
+        Game game = new Game(labs, cars);
+
+        assertRandomNumberInRangeTest(() -> {
+            game.nextLab();
+            assertEquals(1, car1.getPosition());
+            assertEquals(1, car2.getPosition());
+        }, 4);
     }
 }
